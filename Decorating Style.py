04109cf915fc4f45ea5d2f17b1cd5d29ff0f1cc4
file00 <51076009+file00@users.noreply.github.com>
2024-03-05 12:18:00 +0900

    Update and rename Decorating style.py to Decorating Style.py

--- Decorating Style.py
+++ Decorating Style.py
@@ -1,9 +1,8 @@
-# Decorating style
+# Decorating Style
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 class MyApp(QWidget):
-
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -35,7 +34,7 @@ class MyApp(QWidget):
         self.setLayout(vbox)
 
         self.setWindowTitle('Stylesheet')
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(300,300,300,200)
         self.show()
 
 if __name__ == '__main__':
