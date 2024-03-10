658304f6b55f2255d1a4b8bbeebdbe8694c1a24f
file00 <51076009+file00@users.noreply.github.com>
2024-03-11 07:22:30 +0900

    Update QTextBrowser.py

--- QTextBrowser.py
+++ QTextBrowser.py
@@ -10,21 +10,30 @@ class WindowClass(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
 
+        # Code that assigns a function to the button
         self.btn_Print.clicked.connect(self.printTextFunction)
         self.btn_setText.clicked.connect(self.changeTextFunction)
         self.btn_appendText.clicked.connect(self.appendTextFunction)
         self.btn_Clear.clicked.connect(self.clearTextFunction)
 
     def printTextFunction(self) :
+        # self.Textbrowser'name'.toPlainText()
+        # How to get characters in Textbrowser
         print(self.textbrow_Test.toPlainText())
 
     def changeTextFunction(self) :
+        # self.Textbrowser'name'.toPlainText()
+        # How to get characters in Textbrowser
         self.textbrow_Test.setPlainText("This is Textbrowser - Change Text")
 
     def appendTextFunction(self) :
+        # self.Textbrowser'name'.append()
+        # How to get characters in Textbrowser
         self.textbrow_Test.append("Append Text")
 
     def clearTextFunction(self) :
+        # self.Textbrowser.clear()
+        # How to erase letters in Textbrowser
         self.textbrow_Test.clear()
 
 if __name__ == "__main__" :
