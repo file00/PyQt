15f2cadeac4e5ee630eaccd3472ed64780571744
file00 <51076009+file00@users.noreply.github.com>
2024-04-11 16:31:20 +0900

    Add files via upload

---
+++ Basic Image Output.py
@@ -0,0 +1,12 @@
+# Basic Image Output
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+
+app = QtWidgets.QApplication([])
+label = QtWidgets.QLabel()
+pixmap = QtGui.QPixmap('Catnap.png')
+label.setPixmap(pixmap)
+label.resize(pixmap.width(),pixmap.height())
+label.show()
+
+app.exec_()
